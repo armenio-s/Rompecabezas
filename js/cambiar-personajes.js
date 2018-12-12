48ed6b7a95b57bcab69fcf95bf1047114edb8644
Mauricio Armenio <mauricioarmenio@gmail.com>
2018-12-11 23:11:07 -0300

    soluciona problema por el que no se mostraban las fichas

--- js/cambiar-personajes.js
+++ js/cambiar-personajes.js
@@ -2,14 +2,14 @@
 
 function cambiarASamuel() {
 	instrucciones = [];
-	$('#pieza1').attr('src', '/img/samuel/01.png');
-	$('#pieza2').attr('src', '/img/samuel/02.png');
-	$('#pieza3').attr('src', '/img/samuel/03.png');
-	$('#pieza4').attr('src', '/img/samuel/04.png');
-	$('#pieza5').attr('src', '/img/samuel/05.png');
-	$('#pieza6').attr('src', '/img/samuel/06.png');
-	$('#pieza7').attr('src', '/img/samuel/07.png');
-	$('#pieza8').attr('src', '/img/samuel/08.png');
+	$('#pieza1').attr('src', '/img/Samuel/01.png');
+	$('#pieza2').attr('src', '/img/Samuel/02.png');
+	$('#pieza3').attr('src', '/img/Samuel/03.png');
+	$('#pieza4').attr('src', '/img/Samuel/04.png');
+	$('#pieza5').attr('src', '/img/Samuel/05.png');
+	$('#pieza6').attr('src', '/img/Samuel/06.png');
+	$('#pieza7').attr('src', '/img/Samuel/07.png');
+	$('#pieza8').attr('src', '/img/Samuel/08.png');
 	$('#pieza9').attr('src', '/img/glitch.gif');
 	$('#imagen-objetivo').attr('src', '/img/samuel.png');
 	$('#img-objetivo').attr('src', '/img/samuel.png');
@@ -19,14 +19,14 @@ function cambiarASamuel() {
 
 function cambiarAJohn() {
 	instrucciones = [];
-	$('#pieza1').attr('src', '/img/john/01.png');
-	$('#pieza2').attr('src', '/img/john/02.png');
-	$('#pieza3').attr('src', '/img/john/03.png');
-	$('#pieza4').attr('src', '/img/john/04.png');
-	$('#pieza5').attr('src', '/img/john/05.png');
-	$('#pieza6').attr('src', '/img/john/06.png');
-	$('#pieza7').attr('src', '/img/john/07.png');
-	$('#pieza8').attr('src', '/img/john/08.png');
+	$('#pieza1').attr('src', '/img/John/01.png');
+	$('#pieza2').attr('src', '/img/John/02.png');
+	$('#pieza3').attr('src', '/img/John/03.png');
+	$('#pieza4').attr('src', '/img/John/04.png');
+	$('#pieza5').attr('src', '/img/John/05.png');
+	$('#pieza6').attr('src', '/img/John/06.png');
+	$('#pieza7').attr('src', '/img/John/07.png');
+	$('#pieza8').attr('src', '/img/John/08.png');
 	$('#pieza9').attr('src', '/img/glitch.gif');
 	$('#imagen-objetivo').attr('src', '/img/john.png');
 	$('#img-objetivo').attr('src', '/img/john.png');
@@ -35,14 +35,14 @@ function cambiarAJohn() {
 
 function cambiarAUma() {
 	instrucciones = [];
-	$('#pieza1').attr('src', '/img/uma/01.png');
-	$('#pieza2').attr('src', '/img/uma/02.png');
-	$('#pieza3').attr('src', '/img/uma/03.png');
-	$('#pieza4').attr('src', '/img/uma/04.png');
-	$('#pieza5').attr('src', '/img/uma/05.png');
-	$('#pieza6').attr('src', '/img/uma/06.png');
-	$('#pieza7').attr('src', '/img/uma/07.png');
-	$('#pieza8').attr('src', '/img/uma/08.png');
+	$('#pieza1').attr('src', '/img/Uma/01.png');
+	$('#pieza2').attr('src', '/img/Uma/02.png');
+	$('#pieza3').attr('src', '/img/Uma/03.png');
+	$('#pieza4').attr('src', '/img/Uma/04.png');
+	$('#pieza5').attr('src', '/img/Uma/05.png');
+	$('#pieza6').attr('src', '/img/Uma/06.png');
+	$('#pieza7').attr('src', '/img/Uma/07.png');
+	$('#pieza8').attr('src', '/img/Uma/08.png');
 	$('#pieza9').attr('src', '/img/glitch.gif');
 	$('#imagen-objetivo').attr('src', '/img/uma.png');
 	$('#img-objetivo').attr('src', '/img/uma.png');
@@ -51,14 +51,14 @@ function cambiarAUma() {
 
 function cambiarABruce() {
 	instrucciones = [];
-	$('#pieza1').attr('src', '/img/bruce/01.png');
-	$('#pieza2').attr('src', '/img/bruce/02.png');
-	$('#pieza3').attr('src', '/img/bruce/03.png');
-	$('#pieza4').attr('src', '/img/bruce/04.png');
-	$('#pieza5').attr('src', '/img/bruce/05.png');
-	$('#pieza6').attr('src', '/img/bruce/06.png');
-	$('#pieza7').attr('src', '/img/bruce/07.png');
-	$('#pieza8').attr('src', '/img/bruce/08.png');
+	$('#pieza1').attr('src', '/img/Bruce/01.png');
+	$('#pieza2').attr('src', '/img/Bruce/02.png');
+	$('#pieza3').attr('src', '/img/Bruce/03.png');
+	$('#pieza4').attr('src', '/img/Bruce/04.png');
+	$('#pieza5').attr('src', '/img/Bruce/05.png');
+	$('#pieza6').attr('src', '/img/Bruce/06.png');
+	$('#pieza7').attr('src', '/img/Bruce/07.png');
+	$('#pieza8').attr('src', '/img/Bruce/08.png');
 	$('#pieza9').attr('src', '/img/glitch.gif');
 	$('#imagen-objetivo').attr('src', '/img/bruce.png');
 	$('#img-objetivo').attr('src', '/img/bruce.png');
